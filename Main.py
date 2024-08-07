b2f5c98816371af6cb319c0852a51bd57d45a375
volt-l18 <ng05031975@gmail.com>
2024-08-07 12:30:29 +0530

    .

---
+++ Main.py
@@ -0,0 +1,30 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author' : "khushal",
+        'title' : "first post",
+        'content' : "my first post",
+        'date_of_posted' : "feb 18, 2004"
+    },
+    {
+        'author' : "khushal",
+        'title' : "second post",
+        'content' : "my second post",
+        'date_of_posted' : "feb 18, 2004"
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
