c289641ae944c35949072a0ef709271895645b91
volt-l18 <ng05031975@gmail.com>
2024-08-11 00:49:22 +0530

    login form completed(demo login credentials)

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for, flash, redirect
 from Forms import RegistrationFrom , LoginForm
 
 app = Flask(__name__)
@@ -28,14 +28,23 @@ def home():
 def about():
     return render_template('about.html' , title = "About")
 
-@app.route("/register")
+@app.route("/register" , methods = ['GET', 'POST'])
 def register():
     form = RegistrationFrom()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!' , 'success')
+        return redirect(url_for('home'))
     return render_template("register.html" , title = 'Register', form = form)
 
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'abc@abc.com' and form.password.data == 'password':
+            flash('you have been logged in !' , 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Log in unsuccessfull! Please try again" , "danger")
     return render_template("login.html" , title = 'Login', form = form)
 
 if __name__ == "__main__":
