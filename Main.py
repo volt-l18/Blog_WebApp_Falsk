83b5ac7330d6a9e8aba82a1e55b18f07342519ce
volt-l18 <ng05031975@gmail.com>
2024-08-10 23:35:14 +0530

    registration form completetd(login form under work)

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from flask import Flask , render_template , url_for
+from Forms import RegistrationFrom , LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '457e9068bdca2a4d6cdf300d3e510c49'
 
 posts = [
     {
@@ -25,6 +27,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html' , title = "About")
+
+@app.route("/register")
+def register():
+    form = RegistrationFrom()
+    return render_template("register.html" , title = 'Register', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html" , title = 'Login', form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
