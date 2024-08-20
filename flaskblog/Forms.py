25d6188324dc52ce3a1712acef2ba3523ab8bc1d
volt-l18 <ng05031975@gmail.com>
2024-08-20 18:19:04 +0530

    login and regester pages linked with the database (validation error also resolved) logout system created sample account route and page created

--- flaskblog/Forms.py
+++ flaskblog/Forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField , PasswordField , SubmitField , BooleanField
-from wtforms.validators import DataRequired ,Length, Email, EqualTo
+from wtforms.validators import DataRequired ,Length, Email, EqualTo , ValidationError
+from flaskblog.models import User
 
 class RegistrationFrom(FlaskForm):
     username = StringField('Username' , validators =[DataRequired(),Length(min=2,max=20)])
@@ -8,6 +9,16 @@ class RegistrationFrom(FlaskForm):
     password = PasswordField('Password' , validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password' , validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self , username):
+        u = User.query.filter_by(username = username.data).first()
+        if u :
+            raise ValidationError('This Username is already taken')
+
+    def validate_email(self , email):
+        e = User.query.filter_by(username = email.data).first()
+        if e :
+            raise ValidationError('This Email is already taken')
 
 class LoginForm(FlaskForm):
     email = StringField('Email' , validators=[DataRequired(),Email()])
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template , url_for, flash, redirect
-from flaskblog import app
+from flask import render_template , url_for, flash, redirect, Request
+from flask_login.utils import request
+from wtforms.validators import email
+from flaskblog import app , db,  bcrypt
 from flaskblog.models import User, Post
 from flaskblog.Forms import RegistrationFrom , LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -29,19 +32,39 @@ def about():
 
 @app.route("/register" , methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationFrom()
     if form.validate_on_submit():
-        flash(f'Account created for { form.username.data }!' , 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data , email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created!' , 'success')
+        return redirect(url_for('login'))
     return render_template("register.html" , title = 'Register', form = form)
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'abc@abc.com' and form.password.data == 'password':
-            flash('you have been logged in !' , 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password , form.password.data):
+            login_user(user , remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Log in unsuccessfull! Please try again" , "danger")
     return render_template("login.html" , title = 'Login', form = form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template("account.html" , title = 'Account')
