7b34a49998725539d0a9b588f857e4522cec29bb
volt-l18 <ng05031975@gmail.com>
2024-08-30 12:47:01 +0530

    restructured teh application using blueprints also made a config file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,28 +1,35 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '457e9068bdca2a4d6cdf300d3e510c49'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 
-bcrypt = Bcrypt(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-mail = Mail(app)
+mail = Mail()
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from flaskblog import routes
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField , FileAllowed
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
-from wtforms import StringField , PasswordField , SubmitField , BooleanField , TextAreaField
-from wtforms.fields.simple import TextAreaField
-from wtforms.validators import DataRequired ,Length, Email, EqualTo , ValidationError
 from flaskblog.models import User
 
 class RegistrationFrom(FlaskForm):
@@ -47,11 +46,6 @@ class UpdateAccountForm(FlaskForm):
             e = User.query.filter_by(username = email.data).first()
             if e :
                 raise ValidationError('This Email is already taken')
-
-class PostForm(FlaskForm):
-    title = StringField("Title" , validators=[DataRequired()])
-    content = TextAreaField('Content' , validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email' , validators=[DataRequired(),Email()])
