e7104bc25d4488832d472e93b65ae63d91e50481
volt-l18 <ng05031975@gmail.com>
2024-09-02 09:24:45 +0530

    requirements.txt conflict resolved

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,16 @@
 import os
 
+from sqlalchemy.util import non_memoized_property
+#import json
+#
+#with open('/etc/config-flask_blog.json') as config_file:
+#    config = json.load(config_file)
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = "457e9068bdca2a4d6cdf300d3e510c49" #reandoemly genrtated temp key
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = None#config.get('EMAIL_USER')
+    MAIL_PASSWORD = None#config.get('EMAIL_PASS')
